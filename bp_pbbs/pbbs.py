04d8dd07fb4ffb4aa5336bd2b99e896840c9f007
Cheolkie Kim <ckiekim@naver.com>
2022-06-21 23:12:19 +0900

    프로젝트 게시판

--- bp_pbbs/pbbs.py
+++ bp_pbbs/pbbs.py
@@ -6,7 +6,7 @@ from datetime import datetime
 pbbs_bp = Blueprint('pbbs_bp', __name__)
 menu = {'ho':0, 'pb':1, 'm1':0, 'm2':0, 'm3':0, 'cf':0, 'cu':0, 'ma':0}
 
-@pbbs_bp.route('/list/<int:page>', methods=['GET'])
+@pbbs_bp.route('/list/<int:page>')
 def list(page):
     offset = (page - 1) * 10
     count = 3
@@ -24,7 +24,7 @@ def list(page):
                             page_no=page, start_page=start_page, 
                             end_page=end_page, total_page=total_page)
 
-@pbbs_bp.route('/view/<int:pid>', methods=['GET'])
+@pbbs_bp.route('/view/<int:pid>')
 def view(pid):
     authors = [
         {'author':'제임스', 'email':'james@ckworld.com'},
@@ -33,9 +33,12 @@ def view(pid):
     files = ['abc.mp4', '각난닫.pdf']
     title = '손그림 색칠 플라스크 구현'
     desc = '손그림 색칠 플라스크 구현, 손그림 색칠 플라스크 구현'
-    cr_year_month = '2022.06'
+    period = '2022.06'
     view_count = 3
-    course_name = '지능형 빅데이터 서비스 개발(9,10회차)'
+    course_name = '지능형 빅데이터 서비스 개발 과정(9,10회차)'
     course_org = '멀티캠퍼스'
     hash_tags = ['#인공지능','#GAN','#파이썬','#플라스크','#웹']
-    return render_template('pbbs/view.html', menu=menu)
+    row = {'pid':pid, 'title':title, 'desc':desc, 'period':period, 'viewCount':view_count,
+           'course_name':course_name, 'course_org':course_org, 'hash':hash_tags,
+           'authors':authors, 'files':files}
+    return render_template('pbbs/view.html', menu=menu, row=row)
